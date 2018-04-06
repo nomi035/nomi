b248fccbc54c14a0b9c155b35d07ea1a558576b9
nomi035 <noamantariq@gmail.com>
2018-04-06 20:56:04 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var Peer = require('simple-peer')
+var getUserMedia = require('getusermedia')
+
+getUserMedia({ video: true, audio: false }, function (err, stream) {
+  if (err) return console.error(err)
+
+  var Peer = require('simple-peer')
   var peer = new Peer({
     initiator: location.hash === '#init',
     trickle: false,
+    stream: stream
   })
 
   peer.on('signal', function (data) {
@@ -21,3 +27,12 @@ var Peer = require('simple-peer')
   peer.on('data', function (data) {
     document.getElementById('messages').textContent += data + '\n'
   })
+
+  peer.on('stream', function (stream) {
+    var video = document.createElement('video')
+    document.body.appendChild(video)
+
+    video.src = window.URL.createObjectURL(stream)
+    video.play()
+  })
+})
